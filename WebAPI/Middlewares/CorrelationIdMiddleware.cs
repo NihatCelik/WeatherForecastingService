65ea3cdea81ade31d7a943bc64922fa24a3adec5
Nihat Çelik <nc.nihatcelik@hotmail.com>
2025-05-25 21:11:55 +0300

    Integrate Serilog with Elasticsearch and Kibana

---
+++ WebAPI/Middlewares/CorrelationIdMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Serilog.Context;
+using System.Diagnostics;
+
+namespace WebAPI.Middlewares;
+
+public class CorrelationIdMiddleware(RequestDelegate next)
+{
+    private const string CorrelationIdHeader = "X-Correlation-ID";
+
+    public async Task Invoke(HttpContext context)
+    {
+        if (!context.Request.Headers.TryGetValue(CorrelationIdHeader, out var correlationId))
+        {
+            correlationId = Guid.NewGuid().ToString();
+        }
+
+        context.Items[CorrelationIdHeader] = correlationId;
+        context.Response.Headers[CorrelationIdHeader] = correlationId;
+
+        using (LogContext.PushProperty("CorrelationId", correlationId))
+        {
+            await next(context);
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -5,8 +5,34 @@ using MediatR;
 using WebAPI.ServiceCollectionExtensions;
 using Application.Features.WeatherForecast.ValidationRules;
 using WebAPI.Middlewares;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using Serilog.Exceptions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var configuration = new ConfigurationBuilder()
+       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+       .AddJsonFile(
+           $"appsettings.{environment}.json", optional: true
+       ).Build();
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .Enrich.WithExceptionDetails()
+    .WriteTo.Debug()
+    .WriteTo.Console()
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"weather-logs-{DateTime.UtcNow:yyyy.MM}"
+    })
+    .Enrich.WithProperty("Environment", environment)
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -45,10 +71,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<CorrelationIdMiddleware>();
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseMiddleware<RequestLoggingMiddleware>();
 
 app.UseHttpsRedirection();
+
+app.UseSerilogRequestLogging();
 
 app.UseRateLimiter();
 
