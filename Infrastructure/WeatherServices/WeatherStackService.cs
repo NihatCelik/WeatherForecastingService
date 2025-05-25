1979f4e27b1aa8f398d9912e13ba6b6dc5c08567
Nihat Çelik <nc.nihatcelik@hotmail.com>
2025-05-25 17:22:31 +0300

    Add WeatherStackService implementation for GetCurrentWeather

--- Infrastructure/WeatherServices/WeatherStackService.cs
+++ Infrastructure/WeatherServices/WeatherStackService.cs
@@ -1,13 +1,43 @@
-﻿using Domain.Models;
+﻿using Application.Options;
+using Domain.Models;
 using Infrastructure.Abstract;
 using Infrastructure.Enums;
+using Infrastructure.Mapping;
+using Infrastructure.Models;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace Infrastructure.WeatherServices;
 
-public class WeatherStackService : IWeatherService
+public class WeatherStackService(IHttpClientFactory httpClientFactory, IOptions<WeatherStackApiOptions> options) : IWeatherService
 {
-    public Task<WeatherResponse> GetCurrentWeatherAsync(double latitude, double longitude, UnitTypes unitType)
+    private readonly HttpClient _httpClient = httpClientFactory.CreateClient(WeatherStackApiOptions.HttpClientName);
+    private readonly WeatherStackApiOptions _options = options.Value;
+
+    public async Task<WeatherResponse> GetCurrentWeatherAsync(double latitude, double longitude, UnitTypes unitType)
     {
-        throw new NotImplementedException();
+        var url = $"{_options.BaseAddress}?access_key={_options.ApiKey}&query={latitude.ToString().Replace(",", ".")},{longitude.ToString().Replace(",", ".")}";
+        if (unitType != UnitTypes.Default)
+        {
+            var unit = GetUnit(unitType);
+            url += $"&units={unit}";
+        }
+
+        var response = await _httpClient.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+
+        var jsonResult = await response.Content.ReadAsStringAsync();
+        var result = JsonConvert.DeserializeObject<WeatherStackApiResponse>(jsonResult);
+        return WeatherStackMapper.ToWeatherResponse(result);
+    }
+
+    private static string GetUnit(UnitTypes unitType)
+    {
+        return unitType switch
+        {
+            UnitTypes.Metric => "m",
+            UnitTypes.Fahreinheit => "f",
+            _ => "m",
+        };
     }
 }
