54b1390dd71cfc7df72e73b283d64c80574407eb
Nihat Çelik <nc.nihatcelik@hotmail.com>
2025-05-25 17:42:18 +0300

    Add FluentValidation and configure it for input model validation

---
+++ Application/Features/WeatherForecast/ValidationRules/GetWeatherRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.WeatherForecast.Queries.GetWeather;
+using FluentValidation;
+
+namespace Application.Features.WeatherForecast.ValidationRules;
+
+public class GetWeatherRequestValidator : AbstractValidator<GetWeatherQuery>
+{
+    public GetWeatherRequestValidator()
+    {
+        RuleFor(x => x.City)
+            .NotEmpty().WithMessage("City is required")
+            .MinimumLength(3).WithMessage("City name is too short")
+            .MaximumLength(100).WithMessage("City name is too long");
+
+        RuleFor(x => x.Date)
+            .GreaterThan(new DateTime(2000)).WithMessage("The date must be later than January 1, 2000.")
+            .LessThan(new DateTime(2030)).WithMessage("The date must be earlier than January 1, 2030.");
+
+        RuleFor(x => x.UnitType)
+            .IsInEnum().WithMessage("Unit Type must be a valid enum value");
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,9 @@
 using Application.Features.WeatherForecast.Queries.GetCurrentWeather;
+using FluentValidation;
+using MediatR.Extensions.FluentValidation.AspNetCore;
+using MediatR;
 using WebAPI.ServiceCollectionExtensions;
+using Application.Features.WeatherForecast.ValidationRules;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddValidatorsFromAssemblyContaining<GetWeatherRequestValidator>();
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssembly(typeof(GetCurrentWeatherQueryHandler).Assembly));
